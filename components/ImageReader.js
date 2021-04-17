b604adc9f1960ff976f277b95713224153300769
Shruti-prog43 <63506326+Shruti-prog43@users.noreply.github.com>
2021-04-17 18:46:21 +0530

    Update ImageReader.js

---
+++ components/ImageReader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View,Text} from "react-native";
+
+const ImageReader = (props) => {
+    return (
+        <View>
+            <Text>Hello from image reader</Text>
+        </View>
+    );
+};
+
+export default ImageReader;
