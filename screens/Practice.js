6449b64ff85bb5bdab9f3f0ba77bd2376a0a6ee4
ssnuke <49909116+ssnuke@users.noreply.github.com>
2021-04-20 20:21:55 +0530

    Practice updated

--- screens/Practice.js
+++ screens/Practice.js
@@ -1,46 +1,58 @@
-import React,{useState} from "react";
-import { View, Text, StyleSheet,Button,TextInput, FlatList } from "react-native";
-
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 const Practice = (props) => {
-  const [textInput,setTextInput] = useState('');
-  const [data,setNewData] = useState([]);
- 
-  const onAddTask = ()=>{
-    const newData = data.slice();
-    newData.push(textInput);
-    setNewData(newData);
-    console.log(newData);
-  }
-  const item = (data) => {
-    <View>
-      <Text>{data}</Text>
-      
-    </View>
-  }
+  const [textInput, setTextInput] = useState("");
+  const [data, setNewData] = useState([]);
+
+  const changeText = (input) => {
+    setTextInput(input);
+  };
+
+  const onAddTask = () => {
+    setNewData((prevData) => [
+      ...prevData,
+      { id: Math.random().toString(), value: textInput },
+    ]);
+    setTextInput("");
+    console.log(data);
+  };
 
   return (
     <View>
       <Text>New Practice</Text>
       <View style={styles.inputContainer}>
-        <TextInput 
-        style={styles.input}
-        placeholder="ADD A WORD"
-        onChangeText={(text)=>setTextInput(text)}
-        value = {textInput}
-      />
-      <Button
-        style={styles.button}
-        title= "ADD"
-        onPress={onAddTask}
-      />
-      
+        <TextInput
+          style={styles.input}
+          placeholder="ADD A WORD"
+          onChangeText={changeText}
+          value={textInput}
+        />
+        <Button style={styles.button} title="ADD" onPress={onAddTask} />
       </View>
+      {/* <ScrollView>
+        {data.map((item) => (
+          <View key={item} style={styles.listItem}>
+            <Text>{item}</Text>
+          </View>
+        ))}
+      </ScrollView> */}
       <FlatList
-       data = {data}
-       renderItem = {item}
+        data={data}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
       />
-      
     </View>
   );
 };
@@ -48,17 +60,23 @@ const Practice = (props) => {
 export default Practice;
 
 const styles = StyleSheet.create({
-  inputContainer : {
-    flexDirection : 'row',
-    width : '100%',
+  inputContainer: {
+    flexDirection: "row",
+    width: "100%",
   },
-  input :{
-    borderWidth : 1,
-    borderColor : '#ccc',
-    width : '70%',
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    width: "70%",
   },
-  button : {
-    width : '30%',
-  }
-  
+  button: {
+    width: "30%",
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+  },
 });
