0bb7d5b240232b486696e082b31302b0a2ca7a25
Shruti-prog43 <63506326+Shruti-prog43@users.noreply.github.com>
2021-04-25 16:13:25 +0530

    Updated APP.js

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-
-import AppNavigator from  './Navigation/AppNavigator';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import AppNavigator from "./Navigation/AppNavigator";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <AppNavigator/>
+      <AppNavigator />
     </NavigationContainer>
   );
 }
@@ -16,8 +15,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- screens/Reader.js
+++ screens/Reader.js
@@ -1,10 +1,83 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 const Reader = (props) => {
+  const API_KEY = "AIzaSyBw1f7zCf3Ei68jHdLobtRyGGvwjMlk0sU";
+  const API_URL = `https://vision.googleapis.com/v1/images:annotate?key=${API_KEY}`;
+  const [permissions, setPermissions] = useState(false);
+  const [image, setImage] = useState(null);
+  const [status, setStatus] = useState(null);
+
+  const askPermissionsAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    } else {
+      setPermissions(true);
+    }
+  };
+
+  const takePictureAsync = async () => {
+    const { cancelled, uri, base64 } = await ImagePicker.launchCameraAsync({
+      base64: true,
+    });
+
+    if (!cancelled) {
+      setImage(uri);
+      setStatus("Loading...");
+      try {
+        // Call the Google API here
+        const result = await callGoogleVisionAsync(base64);
+        setStatus(result);
+      } catch (error) {
+        setStatus(`Error: ${error.message}`);
+      }
+    } else {
+      setImage(null);
+      setStatus(null);
+    }
+  };
+
+  async function callGoogleVisionAsync(image) {
+    const body = {
+      requests: [
+        {
+          image: {
+            content: image,
+          },
+          features: [
+            {
+              type: "LABEL_DETECTION",
+              maxResults: 1,
+            },
+          ],
+        },
+      ],
+    };
+
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    const result = await response.json();
+    console.log("callGoogleVisionAsync -> result", result);
+
+    return result.responses[0].labelAnnotations[0].description;
+  }
+
   return (
     <View>
-      <Text>New Reader</Text>
+      {image && <Image style={styles.image} source={{ uri: image }} />}
+      {status && <Text style={styles.text}>{status}</Text>}
+      <Button onPress={askPermissionsAsync} title="Ask permissions" />
     </View>
   );
 };
