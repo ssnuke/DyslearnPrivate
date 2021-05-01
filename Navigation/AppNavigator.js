4f9d9ee62f2d3933e4ced2d3fc89f041647c50b1
Shruti-prog43 <63506326+Shruti-prog43@users.noreply.github.com>
2021-05-01 19:39:00 +0530

    updated

--- Navigation/AppNavigator.js
+++ Navigation/AppNavigator.js
@@ -1,22 +1,20 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
-import Home from '../screens/Home';
+import Home from "../screens/Home";
 import Practice from "../screens/Practice";
-import Reader from '../screens/Reader';
-import SigningScreen from '../screens/SigningScreen';
-import CompleteSentence from '../screens/CompleteSentence';
+import Reader from "../screens/Reader";
+import CompleteSentence from "../screens/CompleteSentence";
 
 const AppNavigator = (props) => {
   const Stack = createStackNavigator();
 
   return (
     <Stack.Navigator>
-      <Stack.Screen name='SigningScreen' component={SigningScreen}  />
-      <Stack.Screen name='Home' component={Home}  options={{headerLeft: () => null}}/>
-      <Stack.Screen name='Practice' component={Practice} />
-      <Stack.Screen name='Reader' component={Reader} />
-      <Stack.Screen name='CompleteSentence' component={CompleteSentence} />
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Practice" component={Practice} />
+      <Stack.Screen name="Reader" component={Reader} />
+      <Stack.Screen name="CompleteSentence" component={CompleteSentence} />
     </Stack.Navigator>
   );
 };
