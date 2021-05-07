31f6baa2851d51c3b9caeab73c4b3d39e57493ad
ssnuke <49909116+ssnuke@users.noreply.github.com>
2021-05-07 13:25:17 +0530

    Authentication

--- App.js
+++ App.js
@@ -3,11 +3,14 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./Navigation/AppNavigator";
-import Amplify from "aws-amplify";
-import config from "./aws-exports";
+import Amplify, { Auth } from "aws-amplify";
+import config from  './src/aws-exports';
+import awsconfig from './src/aws-exports';
+import { withAuthenticator } from "aws-amplify-react-native";
 Amplify.configure(config);
+Amplify.configure(awsconfig);
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
       <AppNavigator />
@@ -23,3 +26,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+export default withAuthenticator(App);
--- Navigation/AppNavigator.js
+++ Navigation/AppNavigator.js
@@ -1,17 +1,35 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import { Auth } from "aws-amplify";
 
 import Home from "../screens/Home";
 import Practice from "../screens/Practice";
 import Reader from "../screens/Reader";
 import CompleteSentence from "../screens/CompleteSentence";
+import { Button } from "react-native";
 
 const AppNavigator = (props) => {
   const Stack = createStackNavigator();
 
+  async function signOut() {
+    try {
+      await Auth.signOut();
+    } catch (error) {
+      console.log("error signing out: ", error);
+    }
+  }
+
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerRight: () => {
+            return <Button title="Logout" onPress={signOut}/>;
+          },
+        }}
+      />
       <Stack.Screen name="Practice" component={Practice} />
       <Stack.Screen name="Reader" component={Reader} />
       <Stack.Screen name="CompleteSentence" component={CompleteSentence} />
